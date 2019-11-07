3188b8bc54aa1878d229485dce298b5764b5da5d
melinda <melindad_82@msn.com>
2019-11-07 12:42:12 -0500

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Melinda Diaz on 11/7/19.
+//  Copyright © 2019 Melinda Diaz. All rights reserved.
+//
+//https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/adaptivity-and-layout/
+//HUman Interface Guideline stated in link above
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
